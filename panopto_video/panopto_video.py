6f52e29620099c3eca3ca144a381aabe35c91c60
jperrie <jperrie@isn.edu.au>
2024-10-17 15:47:20 +1100

    Initial commit for Panopto Video XBlock project

---
+++ panopto_video/panopto_video.py
@@ -0,0 +1,63 @@
+"""TO-DO: Write a description of what this XBlock is."""
+
+from importlib.resources import files
+
+from web_fragments.fragment import Fragment
+from xblock.core import XBlock
+from xblock.fields import Integer, Scope
+
+
+class PanoptoVideoXBlock(XBlock):
+    """
+    TO-DO: document what your XBlock does.
+    """
+
+    # Fields are defined on the class.  You can access them in your code as
+    # self.<fieldname>.
+
+    # TO-DO: delete count, and define your own fields.
+    video_url = String(help="The URL for the Panopto video.", default="")
+
+    def resource_string(self, path):
+        """Handy helper for getting resources from our kit."""
+        return files(__package__).joinpath(path).read_text(encoding="utf-8")
+
+    # TO-DO: change this view to display your data your own way.
+    def student_view(self, context=None):
+        html = f"""
+            <div>
+                <iframe src="{self.video_url}" width="640" height="360" allowfullscreen></iframe>
+            </div>
+        """
+        return html
+
+    # TO-DO: change this handler to perform your own actions.  You may need more
+    # than one handler, or you may not need any handlers at all.
+    @XBlock.json_handler
+    def increment_count(self, data, suffix=''):
+        """
+        An example handler, which increments the data.
+        """
+        # Just to show data coming in...
+        assert data['hello'] == 'world'
+
+        self.count += 1
+        return {"count": self.count}
+
+    # TO-DO: change this to create the scenarios you'd like to see in the
+    # workbench while developing your XBlock.
+    @staticmethod
+    def workbench_scenarios():
+        """A canned scenario for display in the workbench."""
+        return [
+            ("PanoptoVideoXBlock",
+             """<panopto_video/>
+             """),
+            ("Multiple PanoptoVideoXBlock",
+             """<vertical_demo>
+                <panopto_video/>
+                <panopto_video/>
+                <panopto_video/>
+                </vertical_demo>
+             """),
+        ]
