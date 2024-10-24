4f4e8a9044ceb120bf6b2eafca3f7fb0fb2ec9a3
jperrie <jperrie@isn.edu.au>
2024-10-24 12:36:31 +1100

    Update on 2024-10-24 12:36:31

--- update.py
+++ update.py
@@ -8,8 +8,9 @@ from datetime import datetime
 def update_tutor_config():
     subprocess.run(["tutor", "config", "save", "--remove", "OPENEDX_EXTRA_PIP_REQUIREMENTS=git+https://github.com/jperrie/panopto_video_xblock.git"], check=True)
     subprocess.run(["tutor", "config", "save", "--append", "OPENEDX_EXTRA_PIP_REQUIREMENTS=git+https://github.com/jperrie/panopto_video_xblock.git"], check=True)
-    subprocess.run(["tutor", "images", "build", "openedx"], check=True)
+    #subprocess.run(["tutor", "images", "build", "openedx"], check=True)
     print("Updated Tutor configuration.")
+    print("Rebuild the image.")
 
 # Main function
 def main():
